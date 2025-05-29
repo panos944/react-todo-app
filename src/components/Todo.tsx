5ffde5ef2136d54989f455ffb8c162a8d3da360f
panos994 <panos.hatzinikolaou@gmail.com>
2025-05-29 18:46:21 +0300

    checkbox for list added

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -18,18 +18,26 @@ const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
         {
           id: Date.now(),
           text: action.payload,
+          completed: false
         }
-      ]
+      ];
 
     case "DELETE":
-      return state.filter(todo => todo.id !== action.payload)
+      return state.filter(todo => todo.id !== action.payload);
 
     case "EDIT":
       return state.map(todo =>
         todo.id === action.payload.id
         ? {...todo, text:action.payload.newText}
         : todo
-      )
+      );
+    
+    case "COMPLETE":
+      return state.map(todo =>
+        todo.id === action.payload
+        ? {...todo,  completed:!todo.completed}
+        : todo
+      );
 
     default:
       return state
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import {Trash2, Edit, Save} from "lucide-react"
+import {Trash2, Edit, Save, Square, X, CheckSquare} from "lucide-react"
 import type { TodoListProps } from "../types"
 import { useState } from "react"
 
@@ -27,51 +27,72 @@ const TodoList = ({todos, dispatch}:TodoListProps) => {
     setEditText("");
   }
 
+  const handleToggle = (id:number) => () => {
+    dispatch({type:"COMPLETE", payload: id})
+  }
+
   return (
   <>
     <ul className="space-y-2">
       {todos.map(todo =>(
         <li key={todo.id} 
-        className="flex items-center justify-between bg-gray-100 p-2 rounded">
+        className={`flex items-center justify-between bg-gray-100 p-2 rounded
+          ${todo.completed ? "opacity-60 line-through" : ""}`}>
 
           {/* HTML for when user toggles edit button */}
           {editId === todo.id ? (
             <>
-            <div className="flex flex-1 gap-2">
-              <input 
-              type="text" 
-              value={editText}
-              className="border rounded p-1"
-              onChange={(e) => setEditText(e.target.value)}/>
-            </div>
+              <div className="flex flex-1 gap-2">
+                <input
+                  type="text"
+                  value={editText}
+                  onChange={(e) => setEditText(e.target.value)}
+                  className="flex-1 border rounded p-1"
+                />
 
-            <div className="flex gap-2">
-              <button
-                onClick={handleSave(todo.id)}
-                className="colour-red-800 text-red-900">
-                <Save size={18}/>
-              </button>
+                <button
+                  onClick={handleSave(todo.id)}
+                  className="text-cf-gray"
+                >
+                  <Save size={18}/>
+                </button>
 
-              <button
-                onClick={handleDelete(todo.id)}
-                className="colour-red-800 text-red-900">
-                <Trash2 size={18}/>
-              </button>
-            </div>
+                <button
+                  onClick={handleCancel}
+                  className="text-cf-dark-red"
+                >
+                  <X size={18}/>
+                </button>
+              </div>
             </>
           ) : (
+            // Rest of the HTML code when not toggling Edit button
               <>
-                <span className="flex-1">{todo.text}</span>
+                <div className="flex items-center gap-2 flex-1">
+                  <button
+                    className="text-green-500"
+                    onClick={handleToggle(todo.id)}
+                  >
+                    {todo.completed ? (
+                      <CheckSquare size={18}/>
+                    ): (
+                      <Square size={18}/>
+                    )}
+                  </button>
+                  <span>{todo.text}</span>
+                </div>
+
                 <div className="flex gap-2">
                   <button
-                    onClick={handleEdit(todo.id, todo.text)}
-                    className="colour-red-800 text-red-900"
+                  onClick={handleEdit(todo.id, todo.text)}
+                  className=" text-red-900"
                   >
-                    <Edit size={18}/>
+                  <Edit size={18}/>
                   </button>
+
                   <button
                     onClick={handleDelete(todo.id)}
-                    className="colour-red-800 text-red-900">
+                    className=" text-red-900">
                     <Trash2 size={18}/>
                   </button>
                 </div> 
